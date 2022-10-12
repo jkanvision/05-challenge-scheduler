aaacc29f85c43e5d4ef86075ff3805a5aeefd549
JK code <112551853+jkanvision@users.noreply.github.com>
2022-10-11 23:09:25 -0400

    retrieves current time in 24hr format and logs; edits html from table to divs; adds event listener to buttons; starts setup of local storage and styling of timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,48 @@
+// variables to manipulate DOM
+var saveBtnEl = $(".saveBtn");
+var taskEntry = $("textarea");
+var timeBlockEl = $("time-block");
+
+
+//creates an array of timeblocks
+// const timeBlockArray = ["9AM", ] 
+
+// adds current date using Moment.js
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+// create a function using Moment's get time command to style time block colors
+var now = moment().hour(Number).format("HH"); {
+    console.log(now);
+    // for (var = i; i < 0; i++); {
+    //     if (now === timeBlockEl.value) {
+    //         this.setAttribute("class", "present");
+    //     }
+    //     else if (now > timeBlockEl.value) {
+    //         this.setAttribute("class", "past");
+    //     }
+    //     else {
+    //         this.setAttribute("class", "future");
+    //     }
+    // }
+
+  
+
+};
+
+
+
+
+
+// store textarea entries into local storage
+function saveTask() {
+    console.log("click");
+    localStorage.setItem("#hour-*", taskEntry);
+    console.log(taskEntry);
+}
+
+// add event listener to save buttons
+saveBtnEl.on("click", saveTask);
+
+// retrieve tasks saved in local storage after page refresh
+$("#hour-*").val(localStorage.getItem("hour-*"));
