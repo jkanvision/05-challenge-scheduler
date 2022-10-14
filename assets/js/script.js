a7112c6a7ba05efdfab2513636210bf36981eff2
JK code <112551853+jkanvision@users.noreply.github.com>
2022-10-14 14:57:05 -0400

    edits to function that colors timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
+$(document).ready(function() {
+
 // variables to manipulate DOM
 var saveBtnEl = $(".saveBtn");
 var taskEntry = $("textarea");
-var timeBlockEl = $("time-block");
+var timeBlockEl = $(".time-block");
 
 
 //creates an array of timeblocks
@@ -11,38 +13,48 @@ var timeBlockEl = $("time-block");
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
 
-// create a function using Moment's get time command to style time block colors
-var now = moment().hour(Number).format("HH"); {
-    console.log(now);
-    // for (var = i; i < 0; i++); {
-    //     if (now === timeBlockEl.value) {
-    //         this.setAttribute("class", "present");
-    //     }
-    //     else if (now > timeBlockEl.value) {
-    //         this.setAttribute("class", "past");
-    //     }
-    //     else {
-    //         this.setAttribute("class", "future");
-    //     }
-    // }
+// creates variable storing current hour in military format
+var now = moment().hours();
 
-  
+// creates a function to color code timeblocks in relation to current hour
+var colorCode = function() {
 
+    $(".time-block").each(function () {
+      console.log($(this));
+      console.log(now);  
+        
+        if (now > parseInt($(this).attr('id').split('hour-')[1])) {
+            $(this).addClass('past');
+        } else if (now === parseInt($(this).attr('id').split('hour-')[1])) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+        } else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+        }
+    })
 };
-
+colorCode();
 
 
 
 
 // store textarea entries into local storage
+// $( this ).text() 
+
 function saveTask() {
     console.log("click");
-    localStorage.setItem("#hour-*", taskEntry);
+    localStorage.setItem("#hour-*", taskEntry.text);
     console.log(taskEntry);
 }
+
+saveTask();
 
 // add event listener to save buttons
 saveBtnEl.on("click", saveTask);
 
 // retrieve tasks saved in local storage after page refresh
 $("#hour-*").val(localStorage.getItem("hour-*"));
+
+})
